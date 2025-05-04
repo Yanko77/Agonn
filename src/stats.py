475e6a41fe6ce41cdfe0696568e1c43ec2ef7d46
Yanko <yanko.lemoine.etu@univ-lille.fr>
2025-05-04 16:02:12 +0200

    delete useless features & rebase Stats class from player.py

--- src/stats.py
+++ src/stats.py
@@ -1,27 +1,22 @@
 import json
 
-from varname import varname
-
-_ALLOWED_C = {'+', '-', '*', '/', '//', '%', ' ', '(', ')', '.'}
+_ALLOWED_C = {'+', '-', '*', '/', '%', ' ', '(', ')', '.'}
 
 
 class Stat:
 
     def __init__(self,
-                 manager: 'Stats'):
+                 manager: 'Stats',
+                 name: str):
         self.manager = manager
         self.owner = manager.owner
 
-        self.name = _get_name()  # stat name = name of the variable the stat object got affected to
+        self.name = name
 
-        try:
-            self.formula = _parse(get_formula(self.name, self.owner.name))
-        except AssertionError:
-            self.formula = ""
+        self.formula = _parse_formula(self.manager.raw_data.get(self.name))
         self._old_formula_list = []
 
         self.flat_bonus = 0
-
 
     @property
     def value(self) -> int:
@@ -54,18 +49,82 @@ class Stat:
 
         self.formula = self._old_formula_list.pop()
 
-
-    def __add__(self, value: int):
+    def add(self, value: int):
         """
         Adds a bonus flat value.
         """
         assert type(value) is int, 'Stat object can only be added with int value'
 
         self.flat_bonus += value
-        return self
 
 
-def _parse(formula: str = ''):
+class Stats:
+    """
+    To read a stat value:
+        Use ``__getitem__`` method
+
+        Example:
+            ```
+            stats = Stats(..., ...)
+            stats['knowledge']
+            ```
+
+    To access a stat:
+        Use ``get`` method
+
+        Example:
+            ```
+            stats = Stats(..., ...)
+            stats.get('knowledge')
+            ```
+
+    To increments a stat:
+        Use ``add`` method of Stat objects
+
+        Example:
+            ```
+            stats = Stats(..., ...)
+            knowledge = stats.get('knowledge')
+            knowledge.add(10)  # Increments knowledge stat by 10 (flat value)
+            ```
+    """
+
+    def __init__(self,
+                 owner: 'Entity'):
+        self.owner = owner
+
+        self.raw_data = get_data(self.owner.class_name)
+
+        stats_name_list = self.raw_data.keys()
+        for stat_name in stats_name_list:
+            setattr(self, stat_name, Stat(self, stat_name))
+
+    def get(self, stat_name:str) -> Stat:
+        """
+        Returns the stat obj of self whose name is ``stat_name``.
+
+        :param stat_name: str
+        :return: Stat object
+        """
+        return self.__getattribute__(stat_name)
+
+    def __getitem__(self, stat_name: str) -> int:
+        """
+        Returns the value of ``stat_name`` in self.
+        """
+        return self.get(stat_name).value
+
+    def locals(self):
+        """
+        Returns locals variables of the class.
+        """
+        return locals()
+
+    @property
+    def list(self) -> tuple:
+        return tuple(attr for attr in self.__dict__.values() if isinstance(attr, Stat))
+
+def _parse_formula(formula: str = ''):
     res = ''
 
     word = ""
@@ -84,29 +143,15 @@ def _parse(formula: str = ''):
 
     return res
 
-
-def _get_name() -> str:
+def get_data(entity_class_name: str) -> dict[str, str]:
     """
-    Used in Stat class __init__ method to get its name.
-    Returns the stat name.
-
-    It uses varname with frame=2.
-
-    :return: str
+    Returns all the stats data of the entity whose class name is ``entity_class_name``.
+    :param entity_class_name: str
+    :return: dict[str, str]
     """
-    name = varname(2)
-
-    if name.startswith('self.'):
-        name = name[5:]
-
-    return name
-
-
-def get_formula(stat_name: str, owner_name: str):
     with open('stats.json', 'r') as file:
-        res = json.load(file)[owner_name].get(stat_name)
+        res = json.load(file).get(entity_class_name)
 
-        assert res is not None, f'Unknown stat : "{stat_name}"'
+        assert res is not None, f'Unknown entity: {entity_class_name}'
 
         return res
-
