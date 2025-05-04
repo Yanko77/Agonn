1431dd274bfea2e77d29b77d5777f77ae886e6f6
Yanko <yanko.lemoine.etu@univ-lille.fr>
2025-05-04 16:01:43 +0200

    create Entity super class

---
+++ src/entity.py
@@ -0,0 +1,15 @@
+from stats import Stats
+
+
+class Entity:
+    """
+    An entity is a character of the game.
+    The player or all mobs are entities objects.
+    """
+    def __init__(self,
+                 game: 'Game',
+                 class_name: str):
+        self.game = game
+        self.class_name = class_name
+
+        self.stats = Stats(self)
