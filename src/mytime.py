3ed505ffc8cb9e6f4b9c2ad72d244a156be29c5a
Yanko <yanko.lemoine.etu@univ-lille.fr>
2025-10-27 16:09:31 +0100

    Add LICENSE & requirements.txt

--- src/mytime.py
+++ src/mytime.py
@@ -1,3 +1,14 @@
+######################################################################
+# Copyright 2025 (c) Yanko Lemoine
+#
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+######################################################################
+
+
 from __future__ import annotations
 import time
 import random
